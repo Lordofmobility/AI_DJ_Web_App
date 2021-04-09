18394e2f744f1b0ef8a373b66f3df53061276322
Ishan Rangnekar <72988033+WhiteStudent@users.noreply.github.com>
2021-04-09 20:56:52 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
-bfunjirdkxndfiqesdkfnqaeiuszxk
+song = ""
+
+function preload() {
+    song = loadSound("Harry_Potter_Theme_Song.mp3")
+}
+
+function setup() {
+    canvas = createCanvas(550, 550)
+    canvas.center()
+
+    video = createCapture(VIDEO)
+    video.hide()
+}
+
+function draw() {
+    image(video, 0, 0, 550, 550)
+}
+
+function play_music() {
+    song.play()
+}
